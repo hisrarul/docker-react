4332d56737ca1e7219edea353f23726a9d972fb5
root <root@localhost.localdomain>
2019-08-26 06:21:23 -0400

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          Hi, I have changed on the feature branch.
         </p>
         <a
           className="App-link"
